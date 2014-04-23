8bd00df8c7aef51c0ddb803a21610c3122e7e426
David Halpern <djh2142@columbia.edu>
2014-04-23 15:40:19 -0400

    Starting Over

---
+++ experiment.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+from numpy import *
+import random
+
+class Experiment(object):
+
+    def __init__(self, params):
+        self.params = params
+        self.paramCounts = {}
+        self.assignments = defaultdict(dict)
+  
+    def assign(self, subject_id):
+        if self.assignments[subject_id]:
+            return self.assignments[subject_id]
+        else:
+            for param in self.params:
+                if len(param.choices) == 0:
+                    continue
+                elif len(param.conditioned_on) == 0:
+                    min_value = min(param.dist.itervalues())
+                    min_keys = [k for k in d if param.dist[k] == min_value]
+                    assignment = random.choice(min_keys)
+                    param.dist[assignment] += 1
+                self.assignments[subject_id] = dict(param=assigment)
+                
+                
+
+
+color = (choices=["red", "green"], conditioned_on=["shape"])
