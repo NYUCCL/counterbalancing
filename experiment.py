2a2366def4db2f2d6fc6eb8245b71ad3e35b1b38
David Halpern <djh2142@columbia.edu>
2014-04-24 18:08:50 -0400

    It works now!

--- experiment.py
+++ experiment.py
@@ -9,48 +9,47 @@ class Experiment(object):
         self.paramCounts = {}
         self.assignments = defaultdict(dict)
         for param in self.params:
-            if len(param.conditioned_on) == 0:
-                self.param.dist = dict.fromkeys(param.choices)
+            if len(param["conditioned_on"]) == 0:
+                param["dist"] = dict.fromkeys(param["choices"], 0)
             else:
                 conditioned_choices = [] 
-                for conditioner in self.param.conditioned_on:
-                    conditioned_choices.append(conditioner.choices)
+                for conditioner in param["conditioned_on"]:
+                    conditioned_choices.append(conditioner["choices"])
                 conditioners = list(product(*conditioned_choices))
-                self.param.dist = dict.fromkeys(conditioners, dict.fromkeys(param.choices))
+                param["dist"] = dict.fromkeys(conditioners, dict.fromkeys(param["choices"], 0))
   
     def assign(self, subject_id):
         unassigned = deque()
         if not self.assignments[subject_id]:
             self.assignments[subject_id] = {}
             for param in self.params:
-                if len(param.choices) != 0:
-                    if len(param.conditioned_on) == 0:
-                        min_value = min(param.dist.itervalues())
-                        min_keys = [k for k in param.dist if param.dist[k] == min_value]
+                if len(param["choices"]) != 0:
+                    if len(param["conditioned_on"]) == 0:
+                        min_value = min(param["dist"].itervalues())
+                        min_keys = [k for k in param["dist"] if param["dist"][k] == min_value]
                         assignment = random.choice(min_keys)
-                        param.dist[assignment] += 1
+                        param["dist"][assignment] += 1
+                        self.assignments[subject_id][param["name"]] = assignment
                     else:
-                        unassigned.appendLeft(param)
-                    self.assignments[subject_id][param] = assignment
+                        unassigned.appendleft(param)
             while unassigned:
                 param = unassigned.pop()
                 conditioned_choices = []
                 has_values = True
-                for conditioner in param.conditioned_on:
-                    if self.assignments[subject_id][conditioner]:
-                        conditioned_choices.append(self.assignments[subject_id][conditioner])
+                for conditioner in param["conditioned_on"]:
+                    if self.assignments[subject_id][conditioner["name"]]:
+                        conditioned_choices.append(self.assignments[subject_id][conditioner["name"]])
                     else:
                         break
                 if has_values:
-                    dist = param.dist[conditioned_choices]
+                    cc = tuple(conditioned_choices)
+                    dist = param["dist"][cc]
                     min_value = min(dist.itervalues())
                     min_keys = [k for k in dist if dist[k] == min_value]
                     assignment = random.choice(min_keys)
-                    param.dist[conditioned_choices][assignment] += 1
-                    self.assignments[subject_id][param] = assignment
+                    param["dist"][cc][assignment] += 1
+                    self.assignments[subject_id][param["name"]] = assignment
         return self.assignments[subject_id]
                 
-
-
 # Example color = (choices=["red", "green"], conditioned_on=["shape"])
 # color.choices = ("red":0, "green":1)
